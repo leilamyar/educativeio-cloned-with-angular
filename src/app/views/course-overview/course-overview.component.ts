4b73f6f182e681a96620c3947b5f2886ba0a0ddb
leilamyar <leilamyar.dev@gmail.com>
2021-11-03 17:35:01 +0100

    feat(overview): html + paths route + unsubscribe

--- src/app/views/course-overview/course-overview.component.ts
+++ src/app/views/course-overview/course-overview.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Course } from 'src/app/domain/Course';
 import { ResourcesService } from 'src/app/services/resources.service';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-course-overview',
   templateUrl: './course-overview.component.html',
   styleUrls: ['./course-overview.component.css']
 })
-export class CourseOverviewComponent implements OnInit {
+export class CourseOverviewComponent implements OnInit, OnDestroy {
 
+  private subsc?: Subscription;
+  private courses: Course[] = [];
+  
   course?: Course;
-  courses: Course[] = [];
 
   constructor(private resourceSv: ResourcesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.resourceSv.getCourses()
+    this.subsc = this.resourceSv.getCourses()
     .subscribe(data => this.courses = data);
     
     let routeParams = this.route.snapshot.paramMap;
     let idFromPrams = Number(routeParams.get('id'));
     
     this.course = this.courses.find(c => c.id === idFromPrams);
+  }
+
+  ngOnDestroy() {
+    this.subsc?.unsubscribe();
   }
 
 }
--- src/app/views/path-overview/path-overview.component.ts
+++ src/app/views/path-overview/path-overview.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Path } from 'src/app/domain/Path';
+import { ResourcesService } from 'src/app/services/resources.service';
 
 @Component({
   selector: 'app-path-overview',
   templateUrl: './path-overview.component.html',
   styleUrls: ['./path-overview.component.css']
 })
-export class PathOverviewComponent implements OnInit {
+export class PathOverviewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subsc?: Subscription;
+  private paths$: Path[] = [];
+  path?: Path;
+
+  constructor(private resourceSv: ResourcesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.subsc = this.resourceSv.getPaths()
+                    .subscribe(data => this.paths$ = data);
+
+    let routeParams = this.route.snapshot.paramMap;
+    let idFromPrams = Number(routeParams.get('id'));
+    this.path = this.paths$.find(p => p.id === idFromPrams);
+  }
+
+  ngOnDestroy() {
+    this.subsc?.unsubscribe();
   }
 
 }
